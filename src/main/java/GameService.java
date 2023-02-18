9de81257c86ad11b4c741995aba28e1a1894feb8
SharkTrader90 <mrimando1990@hotmail.com>
2023-02-18 08:37:39 +0000

    GuessingGame

--- src/main/java/GameService.java
+++ src/main/java/GameService.java
@@ -7,26 +7,21 @@
  * coming mini-projects.
  */
 public class GameService {
-    /**
-     * This variable represents a secret number. This variable can be referenced anywhere within this GameService class.
-     */
+
     public double secretNumber;
 
-    /**
-     * A constructor that will create a GameService object and, by default, set the secretNumber variable to 3.14.
-     * There is no need to change anything in this constructor.
-     */
     public GameService(){
         secretNumber = 3.14;
     }
-
     /**
-     * TODO: this method should return true if guessNumber is equal to the secretNumber, and false otherwise.
-     *
      * @param guessNumber the number that the user has input into their console.
      * @return true if guessNumber is equal to the secretNumber, false otherwise.
      */
     public boolean checkGuess(double guessNumber) {
+        if(guessNumber == secretNumber){
+            return true;
+        } else {
         return false;
+        }
     }
 }
